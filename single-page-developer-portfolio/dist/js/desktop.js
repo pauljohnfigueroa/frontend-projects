b05bd8f2c36b75608ff7f416756c994134619f4c
Paul Figueroa <pjsfigueroa@gmail.com>
2022-11-27 19:39:31 +0800

    Complete Desktop layout Javascript.

--- single-page-developer-portfolio/dist/js/desktop.js
+++ single-page-developer-portfolio/dist/js/desktop.js
@@ -1,6 +1,7 @@
 const projCard = document.querySelectorAll('.card');
 
 projCard.forEach((projCard) => {
+
     const img = projCard.querySelector('.projects-photo');
     const overlay = img.querySelector('.overlay');
     const link = projCard.querySelector('.projects-links');
@@ -8,10 +9,18 @@ projCard.forEach((projCard) => {
     img.addEventListener('mouseover', (e) => {
         overlay.setAttribute('style', 'visibility: visible;');
         link.setAttribute('style', 'visibility: visible;');
-    })
+    });
 
-    img.addEventListener('mouseout', (e) => {
+    // this will keep the efffect consistent when hovering on the Links
+    // card flickers on hover when you remove this.
+    link.addEventListener('mouseover', (e) => {
+        link.setAttribute('style', 'visibility: visible;');
+        overlay.setAttribute('style', 'visibility: visible;');
+    });
+
+    overlay.addEventListener('mouseout', (e) => {
         overlay.setAttribute('style', 'visibility: hidden;');
         link.setAttribute('style', 'visibility: hidden;');
-    })
+    });
+
 });
